2a2d2487768a8775710a38cc493662af873bd2e1
jowy99 <jowycarreras@gmail.com>
2025-01-21 13:04:18 +0100

    little changes

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 import animations from '@midudev/tailwind-animations'
-import typography from 'tailwindcss/typography';
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkmode: 'class',
 	theme: {
 		extend: {
 			colors: {
